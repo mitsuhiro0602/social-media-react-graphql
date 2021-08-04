1fc2ee40eb53405977372ddfe22f7bcb02bdac9a
mitsuhiro0602 <0hornet0@gmail.com>
2021-08-04 13:01:33 +0900

    delete comment

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,10 +1,11 @@
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
   Mutation: {
     createComment: async(_, { postId, body }, context) => {
-      const user = checkAuth(context);
+      const {username} = checkAuth(context);
       if(body.trim() === ''){
         throw new UserInputError('Empty comment', {
           errors: {
@@ -12,7 +13,7 @@ module.exports = {
           }
         })
       }
-      const post = Post.findById(postId);
+      const post = await Post.findById(postId);
 
       if(post){
         post.comments.unshift({
@@ -20,9 +21,28 @@ module.exports = {
           username,
           createdAt: new Date().toISOString()
         })
-        await Post.save();
+        await post.save();
         return post;
       } else throw new UserInputError('Post not found');
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if(post){
+        const commentIndex = post.comments.findIndex(c => c.id === commentId);
+        
+        if(post.comments[commentIndex].username === username){
+          post.comments.splice(commentIndex, 1);
+          await post.save()
+          return post;
+        } else {
+          throw new AuthenticationError('Action not allowed')
+        }
+      } else {
+        throw new UserInputError('Post not found')
+      }
     }
   }
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -44,6 +44,8 @@ module.exports = gql`
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
     createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
 
